8a01a4cd22dbb65dc67d8368f25e0b4431082ec6
SamAbderholden <abderholdens@gmail.com>
2024-03-03 17:03:03 -0700

    Fixed ghostprofile issue

--- SnowDays/Screens/GhostProfile.js
+++ SnowDays/Screens/GhostProfile.js
@@ -1,95 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { getDownloadURL, ref as storageRef } from "firebase/storage";
+import { getDoc, doc, getDocs, query, collection, where } from 'firebase/firestore';
 import FooterButtons from './FooterButtons';
 import Post from '../CustomComponents/Post';
 import { firestore, db } from '../firebase';
-import { getDoc, doc, getDocs, query, collection , where} from 'firebase/firestore';
-import { getDownloadURL, ref as storageRef } from "firebase/storage";
 
 const GhostProfile = ({ route }) => {
-    const userInView = route.params.usertodisplay;
-    const [profileData, setProfileData] = useState(null);
-    const [profileImageUrl, setProfileImageUrl] = useState(null);
-    const [userPosts, setUserPosts] = useState([]);
-    useEffect(() => {
-        const fetchProfileData = async () => {
-            const profileDocRef = doc(firestore, 'profiles', userInView);
-            const profileDocSnap = await getDoc(profileDocRef);
+  const userInView = route.params.usertodisplay;
+  const [profileData, setProfileData] = useState(null);
+  const [profileImageUrl, setProfileImageUrl] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
 
-            if (profileDocSnap.exists()) {
-                const data = profileDocSnap.data();
-                setProfileData(data);
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      const profileDocRef = doc(firestore, 'profiles', userInView);
+      const profileDocSnap = await getDoc(profileDocRef);
 
-                const imageUrl = await getDownloadURL(storageRef(db, `content/${data.profileimage}`));
-                setProfileImageUrl(imageUrl);
-            } else {
-                console.log("No such profile!");
-            }
-        };
+      if (profileDocSnap.exists()) {
+        const data = profileDocSnap.data();
+        setProfileData(data);
+        console.log(data);
+        const imageUrl = await getDownloadURL(storageRef(db, `content/${data.profileImage}`));
+        setProfileImageUrl(imageUrl);
+      } else {
+        console.log("No such profile!");
+      }
+    };
 
-        const fetchUserPosts = async () => {
-            const postsQuery = query(collection(firestore, 'posts'), where('username', '==', userInView));
-            const querySnapshot = await getDocs(postsQuery);
-            const posts = [];
+    const fetchUserPosts = async () => {
+      const postsQuery = query(collection(firestore, 'posts'), where('username', '==', userInView));
+      const querySnapshot = await getDocs(postsQuery);
+      const posts = [];
 
-            for (const doc of querySnapshot.docs) {
-                const postData = doc.data();
-                const fileName = postData.filename;
-                const imageUrl = await getDownloadURL(storageRef(db, `content/${fileName}`));
+      for (const doc of querySnapshot.docs) {
+        const postData = doc.data();
+        const fileName = postData.filename;
+        const imageUrl = await getDownloadURL(storageRef(db, `content/${fileName}`));
 
-                posts.push({ id: doc.id, ...postData, imageUrl, timestamp: postData.timestamp });
-            }
+        posts.push({ id: doc.id, ...postData, imageUrl, timestamp: postData.timestamp });
+      }
 
-            setUserPosts(posts);
-        };
+      setUserPosts(posts);
+    };
 
-        fetchProfileData();
-        fetchUserPosts();
-    }, [userInView]);
+    fetchProfileData();
+    fetchUserPosts();
+  }, [userInView]);
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.username}>@{userInView}</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.username}>@{userInView}</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <View style={styles.rowContainer}>
+          <View style={styles.imageContainer}>
+            <Image
+              style={styles.image}
+              source={profileImageUrl ? { uri: profileImageUrl } : null}
+            />
+          </View>
+          <View style={styles.textFieldsContainer}>
+            <View style={styles.textFieldContainer}>
+              <Text style={styles.label}>Instagram:</Text>
+              <Text style={styles.textField}>{profileData?.instagram}</Text>
             </View>
-            <View style={styles.contentContainer}>
-                <View style={styles.rowContainer}>
-                    <View style={styles.imageContainer}>
-                        <Image
-                            style={styles.image}
-                            source={{ uri: profileImageUrl }}
-                        />
-                    </View>
-                    <View style={styles.textFieldsContainer}>
-                        <View style={styles.textFieldContainer}>
-                            <Text style={styles.label}>Instagram:</Text>
-                            <Text style={styles.textField}>{profileData?.instagram}</Text>
-                        </View>
-                        <View style={styles.textFieldContainer}>
-                            <Text style={styles.label}>Email:</Text>
-                            <Text style={styles.textField}>{profileData?.email}</Text>
-                        </View>
-                        <View style={styles.textFieldContainer}>
-                            <Text style={styles.label}>Gnar Points:</Text>
-                            <Text style={styles.textField}>{profileData?.gnarpoints}</Text>
-                        </View>
-                    </View>
-                </View>
+            <View style={styles.textFieldContainer}>
+              <Text style={styles.label}>Email:</Text>
+              <Text style={styles.textField}>{profileData?.email}</Text>
             </View>
-            <ScrollView style={styles.postsContainer}>
-                {userPosts.map((post) => (
-                    <Post
-                    key={post.id}
-                    imageUrl={post.imageUrl}
-                    description={post.text}
-                    timestamp={post.timestamp}
-                    />
-                ))}
-            </ScrollView>
-            <FooterButtons style={styles.footerButtons}/>
+            <View style={styles.textFieldContainer}>
+              <Text style={styles.label}>Gnar Points:</Text>
+              <Text style={styles.textField}>{profileData?.gnarPoints}</Text>
+            </View>
+          </View>
         </View>
-    );
+      </View>
+      <ScrollView style={styles.postsContainer}>
+        {userPosts.map((post) => (
+          <Post
+            key={post.id}
+            imageUrl={post.imageUrl}
+            description={post.text}
+            timestamp={post.timestamp}
+          />
+        ))}
+      </ScrollView>
+      <FooterButtons style={styles.footerButtons} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
